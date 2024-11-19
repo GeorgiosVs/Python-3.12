6c639feab752da3fbc48880e3eea93c7869541f5
GeorgiosVs <jinje1555@gmail.com>
2024-11-19 18:28:55 +0200

    New inches converter with class import.

--- inches_conv/converter.py
+++ inches_conv/converter.py
@@ -1,11 +1,12 @@
 class Converter:
-    def feet_and_inches_to_cm(self, feet, inches):
+    @staticmethod
+    def feet_and_inches_to_cm(feet, inches):
         cm_per_inch = 2.54
         inches_per_foot = 12
         total_inches = feet * inches_per_foot + inches
         total_cm = total_inches * cm_per_inch
         return total_cm
 
-    def print_conversion(self, feet, inches, cm):
+    @staticmethod
+    def print_conversion(feet, inches, cm):
         print(f"{feet} feet and {inches} inches is equal to {cm:.2f} centimeters")
-        pass
